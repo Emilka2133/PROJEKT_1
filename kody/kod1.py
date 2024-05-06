93ccfe9780ca5211ebbe4e695f68e6b26a75dc9a
Emilka2133 <01179129@pw.edu.pl>
2024-05-06 10:38:14 +0200

    Update kod1.py
    flh na 2000

--- kody/kod1.py
+++ kody/kod1.py
@@ -6,6 +6,7 @@ Created on Mon Apr 22 11:48:51 2024
 """
 
 import numpy as np
+from math import *
 
 class Transformacje:
     def __init__(self,model:str="wgs84"):
@@ -36,29 +37,6 @@ class Transformacje:
                 break
             return(f,l,h) 
 
-if __name__=="__main__":
-    geo=Transformacje(model="wgs84") 
-
-#hirv    
-flh=[]
-with open('wsp_inp.txt','r') as p:
-    linie = p.readlines()
-    wsp = linie[4:]
-    for el in wsp:
-        q=el.split(',')
-        X=float(q[0])
-        Y=float(q[1])
-        Z=float(q[2])
-        f,l,h=geo.hirvonen(X,Y,Z)
-        flh.append([f,l,h])
-
-with open('wyniki_xyz2flh.txt','w') as p:
-    p.write ('f [deg], lam [deg], h [m] \n')
-    for flh_list in flh:
-        line = ','.join([str(wsp)for wsp in flh_list])
-        t = p.writelines(line+'\n')
-
-
 # flh2XYZ
     def nothir (self,f,l,h):
         N=self.a/np.sqrt(1-self.e2*np.sin(f)**2)
@@ -68,27 +46,27 @@ with open('wyniki_xyz2flh.txt','w') as p:
         return(X,Y,Z)  
     
     
-#nothir  
-  
-XYZ=[]
-with open('wyniki_xyz2flh.txt','r') as p:
-    linie = p.readlines()
-    wsp = linie[1:]
-    for el in wsp:
-        q=el.split(',')
-        f=float(q[0])
-        l=float(q[1])
-        h=float(q[2])
-        X,Y,Z=geo.nothir(f,l,h)
-        XYZ.append([X,Y,Z])
+# flh22000
 
-with open('wyniki_flh2xyz.txt','w') as p:
-    p.write ('X [m], Y [m], Z [m] \n')
-    for xyz_list in XYZ:
-        line = ','.join([str(wsp)for wsp in xyz_list])
-        t = p.writelines(line+'\n')
-        
-
+    def flh22000 (self,f,l,l0,nr):
+        A0=1-self.e2/4-3*self.e2**2/64-5*self.e2**3/256
+        A2=(3/8)*(self.e2+self.e2**2/4+15*self.e2**3/128)
+        A4=(15/256)*(self.e2**2+3*self.e2**3/4)
+        A6=(35*self.e2**3)/3072
+        sigma=self.a*(A0*f-A2*np.sin(2*f)+A4*np.sin(4*f)-A6*np.sin(6*f))
+        b2=self.a**2*(1-self.e2)
+        e_2=(self.a**2-b2)/b2
+        dl=l-l0
+        t=np.tan(f)
+        eta2=e_2*(np.cos(f))**2
+        N=self.a/(np.sqrt(1-self.e2*np.sin(f)**2))
+        Xgk=sigma+(dl**2/2)*N*np.sin(f)*np.cos(f)*((1+(dl**2/12)*(np.cos(f))**2*(5-t**2+9*eta2+4*eta2**2)+(dl**4/360)*np.cos(f)**4*(61-58*t**2+t**4+270*eta2-330*eta2*t**2)))
+        Ygk=dl*N*np.cos(f)*(1+(dl**2/6)*(np.cos(f)**2)*(1-t**2+eta2)+((dl**4/120)*(np.cos(f)**4))*(5-18*t**2+t**4+14*eta2-58*eta2*t**2))
+        M2000=0.999923
+        X2000=Xgk*M2000
+        Y2000=Ygk*M2000+nr*1000000+500000
+        return(X2000,Y2000)
+    
 # flh21992
 
     def flh21992 (self,f,l,l0):
@@ -110,6 +88,87 @@ with open('wyniki_flh2xyz.txt','w') as p:
         Y92=Ygk * M1992 + 500000
         return(X92,Y92)
     
+
+
+
+if __name__=="__main__":
+    geo=Transformacje(model="wgs84") 
+
+#hirvonen   
+flh=[]
+with open('wsp_inp.txt','r') as p:
+    linie = p.readlines()
+    wsp = linie[4:]
+    for el in wsp:
+        q=el.split(',')
+        X=float(q[0])
+        Y=float(q[1])
+        Z=float(q[2])
+        f,l,h=geo.hirvonen(X,Y,Z)
+        flh.append([f,l,h])
+
+with open('wyniki_xyz2flh.txt','w') as p:
+    p.write ('f [deg], lam [deg], h [m] \n')
+    for flh_list in flh:
+        line = ','.join([str(wsp)for wsp in flh_list])
+        t = p.writelines(line+'\n')
+
+        
+#nothir  
+  
+XYZ=[]
+with open('wyniki_xyz2flh.txt','r') as p:
+    linie = p.readlines()
+    wsp = linie[1:]
+    for el in wsp:
+        q=el.split(',')
+        f=float(q[0])
+        l=float(q[1])
+        h=float(q[2])
+        X,Y,Z=geo.nothir(f,l,h)
+        XYZ.append([X,Y,Z])
+
+with open('wyniki_flh2xyz.txt','w') as p:
+    p.write ('X [m], Y [m], Z [m] \n')
+    for xyz_list in XYZ:
+        line = ','.join([str(wsp)for wsp in xyz_list])
+        t = p.writelines(line+'\n')
+        
+        
+#2000
+XY2000=[]
+with open('wyniki_xyz2flh.txt','r') as p:
+    linie = p.readlines()
+    wsp = linie[1:]
+    for el in wsp:
+        q=el.split(',')
+        f=float(q[0])
+        l=float(q[1])
+        h=float(q[2])
+        if l >13.5 and l<16.5:
+            l0=15*pi/180
+            nr=5
+        if l >16.5 and l<19.5:
+            l0=18*pi/180
+            nr=6
+        if l >19.5 and l<22.5:
+            l0=21*pi/180
+            nr=7
+        if l >22.5 and l<25.5:
+            l0=24*pi/180
+            nr=8
+        f=f*pi/180
+        l=l*pi/180
+        X2000,Y2000=geo.flh22000(f,l,l0,nr)
+        XY2000.append([X2000,Y2000])
+    
+with open('wyniki_flh22000.txt','w') as p:
+    p.write ('X2000 [m], Y2000[m] \n')
+    for xy2000_list in XY2000:
+        line = ','.join([str(wsp)for wsp in xy2000_list])
+        t = p.writelines(line+'\n')
+        
+        
 #1992       
 XY1992=[]
 with open('wyniki_xyz2flh.txt','r') as p:
